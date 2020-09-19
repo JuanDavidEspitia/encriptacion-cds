6c8a32c3e9d4fba025585b0befd773673a0c3a6e
Juan David Espitia <juandespitia14@gmail.com>
2020-09-19 15:30:12 -0500

    Se cargan las dependencias y archivo base

--- build.sbt
+++ build.sbt
@@ -2,4 +2,14 @@ name := "encriptacion-cds"
 
 version := "0.1"
 
-scalaVersion := "2.13.3"
+scalaVersion := "2.11.12"
+
+val sparkVersion = "2.3.1"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion % Provided,
+  "org.apache.spark" %% "spark-sql" % sparkVersion % Provided,
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % "2.0.0",
+  "com.crealytics" %% "spark-excel" % "0.8.3",
+  "org.apache.spark" %% "spark-mllib" % sparkVersion % "provided")
---
+++ src/main/scala/dataIngest/EncriptedJob.scala
@@ -0,0 +1,27 @@
+package dataIngest
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+object EncriptedJob
+{
+  def main(args: Array[String]): Unit =
+  {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val spark = SparkSession.builder().appName("ETL BA - CDS").master("local[*]").getOrCreate()
+
+    // Cargamos los archivos base a un df
+    val dfSecurity = spark.read
+      .option("header", "true")
+      .option("mergeSchema", "true")
+      .option("delimiter", ";")
+      .csv("input/")
+
+
+
+
+
+
+  }
+
+}
